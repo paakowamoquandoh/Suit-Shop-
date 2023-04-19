0d68b1d6109822340c37dd5060f7f5d9c79ddb79
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-19 20:18:51 +0000

    forms commit

---
+++ public/js/forms.js
@@ -0,0 +1,11 @@
+const submitForm = document.querySelector(".submitButton");
+const nameValue = document.querySelector("#name");
+const emailValue = document.querySelector("#email");
+const passwordValue = document.querySelector("#password");
+const numberValue = document.querySelector("#number");
+const termsAndConditions = document.querySelector("#termsAndConditions");
+const notification = document.querySelector("#notification");
+
+
+
+
