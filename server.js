2f0c33ad0a6ada0299aa97cdffd03be3b77d6d2c
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-19 19:36:04 +0000

    login pade added

--- server.js
+++ server.js
@@ -4,13 +4,33 @@ const admin = require("firebase-admin");
 const bcrypt = require("bcrypt");
 const path = require("path");
 
+//declare static path
+let staticPath = path.join(__dirname, "public");
+
 // express init
 const app = express();
+
+//middlewares
+app.use(express.static(staticPath));
 
 // routes
 //home
 app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(staticPath, "index.html"));
+})
+
+//signup route
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(staticPath, "signup.html"))
+})
+
+//404
+app.get("/404", (req, res) => {
+    res.sendFile(path.join(staticPath, "404.html"));
+})
+
+app.use((req,res) => {
+   res.redirect("/404");
 })
 
 app.listen(3000, () => {
